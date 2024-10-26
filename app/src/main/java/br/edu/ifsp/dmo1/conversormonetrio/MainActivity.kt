d243945a32761e5076b4423fd40117564b548118
Luiz-frj <luizfrj200412@gmail.com>
2024-10-26 18:20:22 -0300

    Primeira Versão do App de conversão monetária

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
@@ -1,20 +1,54 @@
 package br.edu.ifsp.dmo1.conversormonetrio
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.lang.NumberFormatException;
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private val DOLLAR_VALUE = 5.0;
+
+    private lateinit var inputEditText: EditText;
+    private lateinit var calculateButton: Button;
+    private lateinit var outputTextView: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findById();
+        configClikListener();
+    }
+
+    override fun onClick(v: View) {
+        if(v == calculateButton){
+            makeConvertion();
         }
+    }
+
+    private fun findById(){
+        inputEditText = findViewById(R.id.edittext_value);
+        calculateButton = findViewById(R.id.button_calculate);
+        outputTextView = findViewById(R.id.textview_output);
+    }
+
+    private fun configClikListener(){
+        calculateButton.setOnClickListener(this);
+    }
+
+    private fun makeConvertion(){
+        var value = try {
+            inputEditText.text.toString().toDouble();
+        } catch (e: NumberFormatException){
+            0.0;
+        }
+        value = value/DOLLAR_VALUE;
+
+        outputTextView.text = "U$ $value";
     }
 }
