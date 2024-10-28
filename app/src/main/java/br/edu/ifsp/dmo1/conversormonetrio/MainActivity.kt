9b6e97c14a4c13f0e3292c4fac1d97d8b93d03ed
Luiz-frj <luizfrj200412@gmail.com>
2024-10-28 00:16:53 -0300

    Ultima versão do app
    Implementação de outro botão e complexidade desenvolvida

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
@@ -1,20 +1,19 @@
 package br.edu.ifsp.dmo1.conversormonetrio
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import java.lang.NumberFormatException;
-
-
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
     private val DOLLAR_VALUE = 5.0;
 
     private lateinit var inputEditText: EditText;
-    private lateinit var calculateButton: Button;
+    private lateinit var toDollarButton: Button;
+    private lateinit var toRealButton: Button;
     private lateinit var outputTextView: TextView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,30 +24,50 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         configClikListener();
     }
 
+    //onClik => Responsavel por identificar o elemento que foi clicado na interface.
+    //A view clicada é passada como argumento do metodo
+
     override fun onClick(v: View) {
-        if(v == calculateButton){
-            makeConvertion();
+        if(v == toDollarButton){
+            dollarConversion();
+        } else if(v == toRealButton){
+            realConversion();
         }
     }
 
     private fun findById(){
         inputEditText = findViewById(R.id.edittext_value);
-        calculateButton = findViewById(R.id.button_calculate);
+        toDollarButton = findViewById(R.id.button_convert_to_dollar);
+        toRealButton = findViewById(R.id.button_convert_to_real);
         outputTextView = findViewById(R.id.textview_output);
     }
 
+    //Configura como será tratado o clique
+
     private fun configClikListener(){
-        calculateButton.setOnClickListener(this);
+        toRealButton.setOnClickListener(this);
+        toDollarButton.setOnClickListener(this);
     }
 
-    private fun makeConvertion(){
-        var value = try {
+    private fun getValue(): Double{
+        return try {
             inputEditText.text.toString().toDouble();
         } catch (e: NumberFormatException){
             0.0;
         }
-        value = value/DOLLAR_VALUE;
+    }
+
+    private fun dollarConversion(){
+        var value = getValue();
+        value = value / DOLLAR_VALUE;
 
         outputTextView.text = "U$ $value";
+    }
+
+    private fun realConversion(){
+        var value = getValue();
+        value = value * DOLLAR_VALUE;
+
+        outputTextView.text = "R$ $value";
     }
 }
